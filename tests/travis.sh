bb723cc9c7b8ce7850b0816f50308cb7a9b334c4
Emanuele Minotto <minottoemanuele@gmail.com>
2015-12-13 01:07:44 +0100

    allow mongo and redis testing on travis

--- tests/travis.sh
+++ tests/travis.sh
@@ -1,3 +1,4 @@
+# Cassandra installation
 sudo sh -c "echo 'JVM_OPTS=\"\${JVM_OPTS} -Djava.net.preferIPv4Stack=false\"' >> /usr/local/cassandra/conf/cassandra-env.sh"
 sudo service cassandra start
 sudo apt-add-repository -y ppa:linuxjedi/ppa
@@ -6,3 +7,6 @@ sudo apt-get install -y libuv-dev libssl-dev
 cd /tmp && git clone https://github.com/datastax/php-driver.git && cd php-driver && git submodule update --init
 cd ext && ./install.sh && cd "$TRAVIS_BUILD_DIR"
 echo "extension=cassandra.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+# PHP extensions
+yes | pecl install mongo
+yes | pecl install redis
