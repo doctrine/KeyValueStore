1089486506b76d7ed080380bfad1451628d323bb
Emanuele Minotto <minottoemanuele@gmail.com>
2019-10-20 12:40:33 +0200

    DynamoDbStorage internal refactoring

--- lib/Doctrine/KeyValueStore/Storage/DynamoDbStorage.php
+++ lib/Doctrine/KeyValueStore/Storage/DynamoDbStorage.php
@@ -22,40 +22,21 @@ namespace Doctrine\KeyValueStore\Storage;
 
 use Aws\DynamoDb\DynamoDbClient;
 use Aws\DynamoDb\Marshaler;
-use Doctrine\KeyValueStore\InvalidArgumentException;
+use Doctrine\Common\Cache\ArrayCache;
+use Doctrine\Common\Cache\Cache;
 use Doctrine\KeyValueStore\NotFoundException;
 
 /**
- * DyanmoDb storage
+ * DynamoDb storage.
  *
  * @author Stan Lemon <stosh1985@gmail.com>
  */
 class DynamoDbStorage implements Storage
 {
     /**
-     * The key that DynamoDb uses to indicate the name of the table.
+     * @var DynamoDbClient
      */
-    const TABLE_NAME_KEY = 'TableName';
-
-    /**
-     * The key that DynamoDb uses to indicate whether or not to do a consistent read.
-     */
-    const CONSISTENT_READ_KEY = 'ConsistentRead';
-
-    /**
-     * The key that is used to refer to the DynamoDb table key.
-     */
-    const TABLE_KEY = 'Key';
-
-    /**
-     * The key that is used to refer to the marshaled item for DynamoDb table.
-     */
-    const TABLE_ITEM_KEY = 'Item';
-
-    /**
-     * @var \Aws\DynamoDb\DynamoDbClient
-     */
-    protected $client;
+    private $client;
 
     /**
      * @var Marshaler
@@ -63,158 +44,27 @@ class DynamoDbStorage implements Storage
     private $marshaler;
 
     /**
-     * @var string
+     * @var Cache
      */
-    private $defaultKeyName = 'Id';
+    private $descriptionCache;
 
     /**
-     * A associative array where the key is the table name and the value is the name of the key.
-     *
-     * @var array
-     */
-    private $tableKeys = [];
-
-    /**
-     * @param DynamoDbClient $client         The client for connecting to AWS DynamoDB.
-     * @param Marshaler|null $marshaler      (optional) Marshaller for converting data to/from DynamoDB format.
-     * @param string         $defaultKeyName (optional) Default name to use for keys.
-     * @param array          $tableKeys      $tableKeys (optional) An associative array for keys representing table names and values
-     *                                       representing key names for those tables.
+     * @param DynamoDbClient $client           The client for connecting to AWS DynamoDB
+     * @param Marshaler|null $marshaler        (optional) Marshaller for converting data to/from DynamoDB format
+     * @param Cache|null     $descriptionCache Cache used to store tables description
      */
     public function __construct(
-      DynamoDbClient $client,
-      Marshaler $marshaler = null,
-      $defaultKeyName = null,
-      array $tableKeys = []
+        DynamoDbClient $client,
+        Marshaler $marshaler = null,
+        Cache $descriptionCache = null
     ) {
-        $this->client    = $client;
+        $this->client = $client;
         $this->marshaler = $marshaler ?: new Marshaler();
-
-        if ($defaultKeyName !== null) {
-            $this->setDefaultKeyName($defaultKeyName);
-        }
-
-        foreach ($tableKeys as $table => $keyName) {
-            $this->setKeyForTable($table, $keyName);
-        }
+        $this->descriptionCache = $descriptionCache ?: new ArrayCache();
     }
 
     /**
-     * Validates a DynamoDB key name.
-     *
-     * @param $name mixed The name to validate.
-     *
-     * @throws InvalidArgumentException When the key name is invalid.
-     */
-    private function validateKeyName($name)
-    {
-        if (! is_string($name)) {
-            throw InvalidArgumentException::invalidType('key', 'string', $name);
-        }
-
-        $len = strlen($name);
-        if ($len > 255 || $len < 1) {
-            throw InvalidArgumentException::invalidLength('name', 1, 255);
-        }
-    }
-
-    /**
-     * Validates a DynamoDB table name.
-     *
-     * @see http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Limits.html
-     *
-     * @param $name string The table name to validate.
-     *
-     * @throws InvalidArgumentException When the name is invalid.
-     */
-    private function validateTableName($name)
-    {
-        if (! is_string($name)) {
-            throw InvalidArgumentException::invalidType('key', 'string', $name);
-        }
-
-        if (! preg_match('/^[a-z0-9_.-]{3,255}$/i', $name)) {
-            throw InvalidArgumentException::invalidTableName($name);
-        }
-    }
-
-    /**
-     * Sets the default key name for storage tables.
-     *
-     * @param $name string The default name to use for the key.
-     *
-     * @throws InvalidArgumentException When the key name is invalid.
-     */
-    private function setDefaultKeyName($name)
-    {
-        $this->validateKeyName($name);
-        $this->defaultKeyName = $name;
-    }
-
-    /**
-     * Retrieves the default key name.
-     *
-     * @return string The default key name.
-     */
-    public function getDefaultKeyName()
-    {
-        return $this->defaultKeyName;
-    }
-
-    /**
-     * Sets a key name for a specific table.
-     *
-     * @param $table string The name of the table.
-     * @param $key string The name of the string.
-     *
-     * @throws InvalidArgumentException When the key or table name is invalid.
-     */
-    private function setKeyForTable($table, $key)
-    {
-        $this->validateTableName($table);
-        $this->validateKeyName($key);
-        $this->tableKeys[$table] = $key;
-    }
-
-    /**
-     * Retrieves a specific name for a key for a given table. The default is returned if this table does not have
-     * an actual override.
-     *
-     * @param string $tableName The name of the table.
-     *
-     * @return string
-     */
-    private function getKeyNameForTable($tableName)
-    {
-        return isset($this->tableKeys[$tableName]) ?
-          $this->tableKeys[$tableName] :
-          $this->defaultKeyName;
-    }
-
-    /**
-     * Prepares a key to be in a valid format for lookups for DynamoDB. If passing an array, that means that the key
-     * is the name of the key and the value is the actual value for the lookup.
-     *
-     * @param string $storageName Table name.
-     * @param string $key         Key name.
-     *
-     * @return array The key in DynamoDB format.
-     */
-    private function prepareKey($storageName, $key)
-    {
-        if (is_array($key)) {
-            $keyValue = reset($key);
-            $keyName  = key($key);
-        } else {
-            $keyValue = $key;
-            $keyName  = $this->getKeyNameForTable($storageName);
-        }
-
-        return $this->marshaler->marshalItem([$keyName => $keyValue]);
-    }
-
-    /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      */
     public function supportsPartialUpdates()
     {
@@ -222,7 +72,7 @@ class DynamoDbStorage implements Storage
     }
 
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      */
     public function supportsCompositePrimaryKeys()
     {
@@ -230,7 +80,7 @@ class DynamoDbStorage implements Storage
     }
 
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      */
     public function requiresCompositePrimaryKeys()
     {
@@ -238,55 +88,98 @@ class DynamoDbStorage implements Storage
     }
 
     /**
-     * {@inheritDoc}
+     * Prepares a key to be in a valid format for lookups for DynamoDB. If passing an array, that means that the key
+     * is the name of the key and the value is the actual value for the lookup.
+     *
+     * @param string       $storageName Table name
+     * @param array|string $key         Key name
+     *
+     * @return array The key in DynamoDB format
+     */
+    private function prepareKey($storageName, $key)
+    {
+        if (! $this->descriptionCache->contains($storageName)) {
+            $result = $this->client->describeTable([
+                'TableName' => $storageName,
+            ]);
+
+            $keys = isset($result['Table']['KeySchema'])
+                ? $result['Table']['KeySchema']
+                : [];
+            $keys = array_column($keys, 'AttributeName') ?: [];
+
+            $this->descriptionCache->save($storageName, $keys);
+        }
+
+        $keys = isset($keys) ? $keys : $this->descriptionCache->fetch($storageName);
+        $keys = array_combine($keys, array_fill(0, (count($keys) - 1) ?: 1, $key));
+
+        if (!is_array($key)) {
+            $key = [
+                $storageName => $key,
+            ];
+        }
+
+        $keys = array_intersect_assoc($keys, $key) ?: $keys;
+
+        return $this->marshaler->marshalItem($keys);
+    }
+
+    /**
+     * {@inheritdoc}
      */
     public function insert($storageName, $key, array $data)
     {
         $this->client->putItem([
-          self::TABLE_NAME_KEY => $storageName,
-          self::TABLE_ITEM_KEY => $this->prepareKey($storageName, $key) + $this->marshaler->marshalItem($this->prepareData($data)),
+            'TableName' => $storageName,
+            'Item' => $this->prepareKey($storageName, $key) + $this->marshaler->marshalItem($data),
         ]);
     }
 
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      */
     public function update($storageName, $key, array $data)
     {
-        //We are using PUT so we just replace the original item, if the key
-        //does not exist, it will be created.
-        $this->insert($storageName, $key, $this->prepareData($data));
+        // we are using PUT so we just replace the original item, if the key
+        // does not exist, it will be created.
+        $this->insert($storageName, $key, $data);
     }
 
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      */
     public function delete($storageName, $key)
     {
         $this->client->deleteItem([
-          self::TABLE_NAME_KEY => $storageName,
-          self::TABLE_KEY      => $this->prepareKey($storageName, $key),
+            'Key' => $this->prepareKey($storageName, $key),
+            'TableName' => $storageName,
         ]);
     }
 
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      */
     public function find($storageName, $key)
     {
+        $keys = $this->prepareKey($storageName, $key);
+
         $item = $this->client->getItem([
-          self::TABLE_NAME_KEY      => $storageName,
-          self::CONSISTENT_READ_KEY => true,
-          self::TABLE_KEY           => $this->prepareKey($storageName, $key),
+            'ConsistentRead' => true,
+            'Key' => $keys,
+            'TableName' => $storageName,
         ]);
 
-        if (! $item) {
+        if (! $item->hasKey('Item')) {
             throw NotFoundException::notFoundByKey($key);
         }
 
-        $item = $item->get(self::TABLE_ITEM_KEY);
+        $item = $item->get('Item');
 
-        return $this->marshaler->unmarshalItem($item);
+        $result = $this->marshaler->unmarshalItem($item);
+        $result = array_diff_key($result, $keys);
+
+        return $result;
     }
 
     /**
@@ -297,26 +190,5 @@ class DynamoDbStorage implements Storage
     public function getName()
     {
         return 'dynamodb';
-    }
-
-    /**
-     * Prepare data by removing empty item attributes.
-     *
-     * @param array $data
-     *
-     * @return array
-     */
-    protected function prepareData($data)
-    {
-        $callback = function ($value) {
-            return $value !== null && $value !== [] && $value !== '';
-        };
-
-        foreach ($data as &$value) {
-            if (is_array($value)) {
-                $value = $this->prepareData($value);
-            }
-        }
-        return array_filter($data, $callback);
     }
 }
